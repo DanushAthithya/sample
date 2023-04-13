fd10e6c61b62d6bde140bd46a57f6dd659553a73
DanushAthithya <83062926+DanushAthithya@users.noreply.github.com>
2023-04-13 15:32:59 +0530

    Update ceaser.php

--- ceaser.php
+++ ceaser.php
@@ -9,19 +9,16 @@ for($i=0;$i<$coun;$i++)
     if($text[$i]!=" ")
     {
         $in=intval($text[$i]);
-        echo $in;
         $in=$in+$key;
         if($in>122)
         {
             $k=$in-122;
             $in=96+$k;
-            echo $in;
         }
         else if($in>90 && $in<97)
         {
             $k=$in-90;
             $in=64+$k;
-            echo $in;
         }
         $st=chr($in);
         $text[$i]=$st;
